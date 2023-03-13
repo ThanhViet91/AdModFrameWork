7414b0e7f0e93d8a8b5f4fc14e1185f377f7ca8a
ThanhLV <infoboy.ftu.edu@gmail.com>
2023-03-13 18:54:47 +0700

    - start ver 1.0.2

--- app/src/main/java/com/thanhlv/adsframework/AdMod.java
+++ app/src/main/java/com/thanhlv/adsframework/AdMod.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.thanhlv.mylibrary.AdsConfigs;
+
 public class AdMod extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
+
     }
 }
