51da059b5bbecf4e23eec4a4b4314787143f8ffc
ThanhLV <infoboy.ftu.edu@gmail.com>
2023-03-13 15:47:31 +0700

    - start code 5

--- admodframework/src/main/java/com/thanhlv/admodframework/AdMod.java
+++ admodframework/src/main/java/com/thanhlv/admodframework/AdMod.java
@@ -1,4 +1,10 @@
 package com.thanhlv.admodframework;
 
 public class AdMod {
+    public void testPublicVoid(){}
+    private void testPrivateVoid() {}
+    public static void testPublicStaticVoid() {}
+    public static int testPublicStaticInt;
+    public int testPublicInt;
+    private int testPrivatecInt;
 }
