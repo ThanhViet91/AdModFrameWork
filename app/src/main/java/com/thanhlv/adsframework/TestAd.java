d38855df9f52d37e1dba76ea36619e85d141dca5
ThanhLV <infoboy.ftu.edu@gmail.com>
2023-03-13 15:24:29 +0700

    - start code 2

---
+++ app/src/main/java/com/thanhlv/adsframework/TestAd.java
@@ -0,0 +1,12 @@
+package com.thanhlv.adsframework;
+
+public class TestAd {
+    public void start(){
+
+    }
+
+    public static void startStatic(){}
+
+    public int varPublic;
+    public static int varPublicStatic;
+}
